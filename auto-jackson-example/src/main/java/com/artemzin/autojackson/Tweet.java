80b0f1c96f2b99eb18f2c9ddfef4a86894bfa227
Yi Ou <ouyi@gmx.de>
2016-12-31 11:27:33 +0000

    Remove Nonnull, which is the default AutoValue

--- auto-jackson-example/src/main/java/com/artemzin/autojackson/Tweet.java
+++ auto-jackson-example/src/main/java/com/artemzin/autojackson/Tweet.java
@@ -5,24 +5,20 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.google.auto.value.AutoValue;
 
-import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
 @AutoValue
 @JsonDeserialize(builder = AutoValue_Tweet.Builder.class)
 public abstract class Tweet {
 
-  @Nonnull
   @JsonCreator
   public static Builder builder() {
     return new AutoValue_Tweet.Builder().blocked(false);
   }
 
-  @Nonnull
   @JsonProperty("author")
   public abstract String author();
 
-  @Nonnull
   @JsonProperty("content")
   public abstract String content();
 
@@ -31,19 +27,17 @@ public abstract class Tweet {
 
   @AutoValue.Builder
   public static abstract class Builder {
-    @Nonnull
+
     @JsonProperty("author")
-    public abstract Builder author(@Nonnull String author);
+    public abstract Builder author(String author);
 
-    @Nonnull
     @JsonProperty("content")
-    public abstract Builder content(@Nonnull String content);
+    public abstract Builder content(String content);
 
-    public abstract Builder blocked(@Nonnull Boolean blocked);
+    public abstract Builder blocked(Boolean blocked);
 
     public abstract Boolean blocked();
 
-    @Nonnull
     public abstract Tweet autoBuild();
 
     public Tweet build() {
